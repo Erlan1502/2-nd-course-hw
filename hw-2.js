5afdbb1b28e4065860f0fb7d07f192cbf63e69d7
erlasha07@gmail.com <erlasha07@gmail.com>
2024-09-15 20:51:59 +0500

    hw-2 complete

---
+++ hw-2.js
@@ -0,0 +1,46 @@
+//1
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+//2
+const yearOfRelease = 2007;
+alert(yearOfRelease);
+//3
+const nameOfCreatorJS = "Брендан Эйх";
+alert(nameOfCreatorJS);
+//4
+let firstNumber = 10;
+let secondNumber = 2;
+alert(firstNumber + secondNumber);
+alert(firstNumber - secondNumber);
+alert(firstNumber * secondNumber);
+alert(firstNumber / secondNumber);
+//5
+let result = 2 ** 5;
+alert(result);
+//6
+let newA = 9;
+let b = 2;
+alert(a % b);
+//7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+//8
+let age = prompt("Сколько вам лет?");
+alert(`Вам ${age} лет`);
+//9
+const user = {
+  name: "Erlan",
+  age: 25,
+  isAdmin: true,
+};
+//10
+let userName = prompt("Как ва зовут?");
+alert(`Вас зовут ${userName}`);
